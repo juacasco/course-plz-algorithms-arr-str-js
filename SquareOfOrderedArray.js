42278a7912fe59bfd55f69cd671cc82c502e9cf1
Juan Pablo Carrasco <juacasco@gmail.com>
2023-04-08 09:28:04 -0500

    Fixed with some kind of ugly code the square of sorted array problem -- could be enhanced by changing loop type

--- SquareOfOrderedArray.js
+++ SquareOfOrderedArray.js
@@ -1,20 +1,20 @@
 function getSquaresOrderedArray(inputArray){
     let p1 = 0;
     let p2 = inputArray.length -1;
-
+    let returnArr = [];
+    let rp2 = inputArray.length -1;
     while (p1<=p2) {
         let sqP1 = inputArray[p1]**2;
         let sqP2 = inputArray[p2]**2;
+        returnArr[rp2] = Math.max(sqP1, sqP2);
         if (sqP1>sqP2) { // p1 es más grande
-            inputArray[p1] = inputArray[p2];
-            inputArray[p2] = sqP1;
-            p2--;
-        }else{ // p2 es más grande o igual
-            inputArray[p2] = sqP2;
+            p1++;
+        } else{
             p2--;
         }
+        rp2--;
     }
-    return inputArray
+    return returnArr;
 }
 
 let inputA = [-4,-1,0,3,10];
