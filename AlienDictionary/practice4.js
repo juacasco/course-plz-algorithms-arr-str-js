33cbca3dd1177e9b808d754fd8272e0c4608208c
Juan Pablo Carrasco <jp.sw.eng@outlook.com>
2024-01-10 19:40:32 +0000

    Adding practice 4. Still more practice is required, and still need to revise improvements oportunities. Almost ready to begin with other algorithms and languages

---
+++ AlienDictionary/practice4.js
@@ -0,0 +1,29 @@
+function isProperlySorted(alphabet, listWords){
+    const alphabetMap = new Map();
+    for (let i = 0; i < alphabet.length; i++){
+        alphabetMap[alphabet[i]] = i;
+    }
+    const compareWordsOrder = function (w1, w2){
+        minWordLen = Math.min(w1.length, w2.length);
+        for(let i = 0; i<minWordLen;i++){
+            if(alphabetMap[w1[i]] < alphabetMap[w2[i]]){
+                return true;
+            }
+            if(alphabetMap[w1[i]] > alphabetMap[w2[i]]){
+                return false;
+            }
+        }
+        return w1.length <= w2.length;
+    }
+    for (let i=1; i<listWords.length;i++){
+        if(compareWordsOrder(listWords[i-1], listWords[i]) === false){
+            return false;
+        }
+    }
+    return true;
+}
+
+const words1 = ["habito", "hacer", "lectura", "sonreir"];
+const order1 = "hlabcdfgijkmnopqrstuvwxyz";
+
+console.log(isProperlySorted(order1,words1));
